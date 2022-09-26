5e9b3a2d1700e7b5ef8cb5c1ff6316ca11822274
Jhonatan Thallisson Cabral Néry <jhonatanthallisson@gmail.com>
2022-09-26 17:09:05 -0300

    Tradução e ajustes

--- MeuSite/signUp/script.js
+++ MeuSite/signUp/script.js
@@ -1,6 +1,7 @@
 // SignUp Screen
 
-const myInput = document.getElementById("passwordField1");
+const myPassword = document.getElementById("passwordField1");
+const myConfirmPassword = document.getElementById("passwordField2");
 const letter = document.getElementById("letter");
 const capital = document.getElementById("capital");
 const number = document.getElementById("number");
@@ -8,85 +9,91 @@ const length = document.getElementById("length");
 
 
 // When the user clicks on the password field, show the message box
-myInput.onfocus = function () {
-  document.getElementById("message").style.display = "block";
-}
-
-// When the user clicks outside of the password field, hide the message box
-myInput.onblur = function () {
-  document.getElementById("message").style.display = "none";
+myPassword.onfocus = function() {
+    document.getElementById("message").style.display = "block";
 }
 
 // When the user starts to type something inside the password field
-myInput.onkeyup = function () {
-  // Validate lowercase letters
-  var lowerCaseLetters = /[a-z]/g;
-  if (myInput.value.match(lowerCaseLetters)) {
-    letter.classList.remove("invalid");
-    letter.classList.add("valid");
-  } else {
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
-  }
+myPassword.onkeyup = function() {
+    // Validate lowercase letters
+    var lowerCaseLetters = /[a-z]/g;
+    if (myPassword.value.match(lowerCaseLetters)) {
+        letter.classList.remove("invalid");
+        letter.classList.add("valid");
+    } else {
+        letter.classList.remove("valid");
+        letter.classList.add("invalid");
+    }
 
-  // Validate capital letters
-  var upperCaseLetters = /[A-Z]/g;
-  if (myInput.value.match(upperCaseLetters)) {
-    capital.classList.remove("invalid");
-    capital.classList.add("valid");
-  } else {
-    capital.classList.remove("valid");
-    capital.classList.add("invalid");
-  }
+    // Validate capital letters
+    var upperCaseLetters = /[A-Z]/g;
+    if (myPassword.value.match(upperCaseLetters)) {
+        capital.classList.remove("invalid");
+        capital.classList.add("valid");
+    } else {
+        capital.classList.remove("valid");
+        capital.classList.add("invalid");
+    }
 
-  // Validate numbers
-  var numbers = /[0-9]/g;
-  if (myInput.value.match(numbers)) {
-    number.classList.remove("invalid");
-    number.classList.add("valid");
-  } else {
-    number.classList.remove("valid");
-    number.classList.add("invalid");
-  }
+    // Validate numbers
+    var numbers = /[0-9]/g;
+    if (myPassword.value.match(numbers)) {
+        number.classList.remove("invalid");
+        number.classList.add("valid");
+    } else {
+        number.classList.remove("valid");
+        number.classList.add("invalid");
+    }
 
-  // Validate length
-  if (myInput.value.length >= 8) {
-    length.classList.remove("invalid");
-    length.classList.add("valid");
-  } else {
-    length.classList.remove("valid");
-    length.classList.add("invalid");
-  }
+    // Validate length
+    if (myPassword.value.length >= 8) {
+        length.classList.remove("invalid");
+        length.classList.add("valid");
+    } else {
+        length.classList.remove("valid");
+        length.classList.add("invalid");
+    }
+}
+
+myConfirmPassword.onkeyup = function() {
+    let registeredPassword = document.getElementById("passwordField1").value;
+    let confirmedPassword = document.getElementById("passwordField2").value;
+
+    if (confirmedPassword != registeredPassword) {
+        return;
+    }
+    myConfirmPassword.classList = "valid";
 }
 
 function setRegistration() {
-  let registeredUsername = document.getElementById("usernameSignUp").value;
-  let registeredPassword = document.getElementById("passwordField1").value;
-  let confirmedPassword = document.getElementById("passwordField2").value;
-
-  if (letter.classList == "valid"
-    && capital.classList == "valid"
-    && number.classList == "valid"
-    && length.classList == "valid") {
-
-    if (confirmedPassword != registeredPassword) {
-      alert("A confirmação precisa ser igual a senha!");
-      return;
-    }
+    let registeredUsername = document.getElementById("usernameSignUp").value;
+    let registeredPassword = document.getElementById("passwordField1").value;
+    let confirmedPassword = document.getElementById("passwordField2").value;
 
     if (registeredUsername == "") {
-      alert("O usuário não pode estar vazio!");
-      return;
+        alert("O usuário não pode estar vazio!");
+        return;
     }
 
-    sessionStorage.setItem('userName', registeredUsername);
-    sessionStorage.setItem('password', registeredPassword);
+    if (confirmedPassword != registeredPassword) {
+        alert("A confirmação precisa ser igual a senha!");
+        return;
+    }
 
-    alert("Usuário e senha gravados com sucesso!");
+    if (letter.classList == "valid" &&
+        capital.classList == "valid" &&
+        number.classList == "valid" &&
+        length.classList == "valid") {
 
-    window.location.href = "../login/login.html";
+        sessionStorage.setItem('userName', registeredUsername);
+        sessionStorage.setItem('password', registeredPassword);
 
-  } else {
-    alert("Preencha os campos corretamente!");
-  }
+        alert("Usuário e senha gravados com sucesso!");
+
+        window.location.href = "../login/login.html";
+
+    } else {
+        alert("Preencha a senha corretamente!");
+    }
+
 }
